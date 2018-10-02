78b0ce12ecf015ef2b6e7642e23ffa26cc6e1922
mezeinikolett <43540395+mezeinikolett@users.noreply.github.com>
2018-10-02 09:12:56 +0200

    Add files via upload

---
+++ 1.f/src/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Niki
+ */
+public class Main {
+    public static void main(String[] args) {
+         Pont p1= new Pont(4.6,6,3,8);
+         Pont p2= new Pont(13, 24.3, 32,64);
+         Pont p3= new Pont(20.8, 16, 49, 51);
+         Pont p4= new Pont(14, 93, 22, 79.12);
+         
+        System.out.println("x1="+p1.getPont1()+" "+"x2="+p1.gePont2()+"y1="+p1.getPont1()+" "+"y2="+p1.gePont2());
+        System.out.println("x1="+p2.getPont1()+" "+"x2="+p2.getPont2()"y1="+p2.getPont1()+" "+"y2="+p2.gePont2());
+        System.out.println("x1="+p3.getPont1()+" "+"x2="+p3.gePont2()+"y1="+p3.getPont1()+" "+"y2="+p3.gePont2());
+        System.out.println("x1="+p4.getPont1()+" "+"x2="+p4.getPont2()"y1="+p4.getPont1()+" "+"y2="+p4.gePont2());
+        
+            
+    
+    
+    
+    
+    }
+    
+}
